39a9d21b73e06260460fdc7375833d5fb6e62d6e
Shrayan Bhattacharya <shrayanjob08@gmail.com>
2022-04-13 12:30:04 +0530

    Added Selection sort and Bubble sort

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,7 +1,4 @@
-/**
- * BubbleSort
- */
-public class BubbleSort {
+public class SelectionSort {
     public static void printArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]+" ");
@@ -11,14 +8,18 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int arr[]={7,8,3,1,2};
+
+        
         for (int i = 0; i <= arr.length-1; i++) {
-            for (int j = 0; j < arr.length-1-i; j++) {
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+            int smallest=i;
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[smallest]>arr[j]){
+                    smallest=j;
                 }
             }
+            int temp=arr[smallest];
+            arr[smallest]=arr[i];
+            arr[i]=temp;
         }
         printArray(arr);
     }
